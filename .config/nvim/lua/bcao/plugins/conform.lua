535afba77647f0001986b1bf40fd1f664585b025
Bang Cao <caothanhbang2002@gmail.com>
2024-05-13 22:33:25 -0400

    update config

---
+++ .config/nvim/lua/bcao/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        cpp = { "clang-format" },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 2000,
+        lsp_fallback = true,
+      },
+    })
+  end,
+}
