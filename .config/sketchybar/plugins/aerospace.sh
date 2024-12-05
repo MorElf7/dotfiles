a7722c98b120f1d3b2ed9008ed2f47f6f9c8447c
Steve Cao <steve.cao4502@gmail.com>
2024-12-04 20:41:33 -0500

    update sketchybar config to match the looks on linux

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+source "$CONFIG_DIR/colors.sh"
 
 FOCUSED_WORKSPACE="$(aerospace list-workspaces --focused)"
-sketchybar --set $NAME label="[ $FOCUSED_WORKSPACE ]" 
+# sketchybar --set $NAME label="$FOCUSED_WORKSPACE"
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+    sketchybar --set $NAME background.color=$MAGENTA label.color=$BG0 label.shadow.drawing=off
+else
+    sketchybar --set $NAME background.color=$BG0 label.color=$WHITE
+fi
