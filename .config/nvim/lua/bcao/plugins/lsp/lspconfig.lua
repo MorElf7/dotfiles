93e1060c956744ce1d901ade30e60afe0212bbbf
Steve Cao <steve.cao4502@gmail.com>
2025-04-07 23:48:23 -0400

    feat: migrate to nvim.diagnostic.config

--- .config/nvim/lua/bcao/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/bcao/plugins/lsp/lspconfig.lua
@@ -61,18 +61,28 @@ return {
         local capabilities = require("blink.cmp").get_lsp_capabilities()
 
         -- Change the Diagnostic symbols in the sign column (gutter)
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
-
-        -- -- configure eslint server
-        -- lspconfig["eslint-lsp"].setup({
-        --   capabilities = capabilities,
-        --   on_attach = on_attach,
-        --   filetypes = { "javascript", "typescript" },
-        -- })
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.HINT] = "󰠠 ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                },
+                linehl = {
+                    [vim.diagnostic.severity.ERROR] = "",
+                    [vim.diagnostic.severity.WARN] = "",
+                    [vim.diagnostic.severity.HINT] = "",
+                    [vim.diagnostic.severity.INFO] = "",
+                },
+                numhl = {
+                    [vim.diagnostic.severity.ERROR] = "",
+                    [vim.diagnostic.severity.WARN] = "",
+                    [vim.diagnostic.severity.HINT] = "",
+                    [vim.diagnostic.severity.INFO] = "",
+                },
+            },
+        })
 
         -- configure html server
         lspconfig["html"].setup({
